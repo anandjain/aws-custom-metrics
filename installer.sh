5b071e151eeadc3f8932aef07c9b3b1fd3b8e271
anandjain <anandjain420@gmail.com>
2017-03-03 11:56:23 +0000

    updated installer 1

--- installer.sh
+++ installer.sh
@@ -5,18 +5,26 @@ OS_NAME=`cat /etc/*release | grep -i "^NAME" | cut -d"=" -f2`
 case $OS_NAME in
         '"Ubuntu"')
                 sudo apt-get update -y; sudo apt-get install git unzip awscli wget -y
-                wget https://raw.githubusercontent.com/anandjain/aws-custom-metrics/master/new_script.sh  
-                sh new_script.sh
+		git clone https://github.com/anandjain/aws-custom-metrics.git
         ;;
         '"Debian"')
                 sudo apt-get update -y ; sudo apt-get install git unzip awscli wget -y
-                wget https://raw.githubusercontent.com/anandjain/aws-custom-metrics/master/new_script.sh 
-                sh new_script.sh
+		git clone https://github.com/anandjain/aws-custom-metrics.git
         ;;
         *)
                 sudo yum update -y ; sudo yum install git unzip wget -y
-                wget https://raw.githubusercontent.com/anandjain/aws-custom-metrics/master/new_script.sh 
-                sh new_script.sh
+		# Configure AWS-CLI
+                echo "Installing aws-cli..."
+                curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+                unzip awscli-bundle.zip
+                sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+                sudo rm awscli-bundle.zip
+                # Configure AWS-CLI
+                if [ ! -d ~/.aws ]
+                   then
+                        mkdir -p ~/.aws
+                fi
+		git clone https://github.com/anandjain/aws-custom-metrics.git
         ;;
 esac
 
